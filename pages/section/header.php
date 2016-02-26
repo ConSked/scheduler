36f1f439614249d9705ed36cc8203731868bf6c4
Earl Gero <ecgero@comcast.net>
2016-02-26 13:22:40 -0600

    Google ads added to header

--- pages/section/header.php
+++ pages/section/header.php
@@ -1,7 +1,33 @@
 <?php /* $Id: header.php 2005 2012-09-17 16:49:22Z ecgero $ Copyright (c) ConSked, LLC. All Rights Reserved. */ ?>
 
 <div id="header">
-	<!-- Begin Header Image -->
-	<img src="images/intlogo.png" width="344" height="84" alt="" />
-	<!-- End Header Image -->
+    <table style="width: 100%">
+        <tr>
+            <td style="vertical-align: middle; text-align: left; width: 33%">
+	            <img src="images/intlogo.png" width="344" height="84" alt="" />
+            </td>
+            <td style="vertical-align: middle; text-align: center; width: 34%">
+                <div style="width: 468pt">
+                    <script type="text/javascript">
+                        <!--
+                        google_ad_client = "pub-8102958922361899";
+                        //google_ad_width = 728;
+                        //google_ad_height = 90;
+                        //google_ad_format = "728x90_as";
+                        google_ad_width = 468;
+                        google_ad_height = 60;
+                        google_ad_format = "468x60_as";
+                        google_ad_type = "text_image";
+                        google_ad_channel = "";
+                        -->
+                    </script>
+                    <script type="text/javascript"
+                        src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+                    </script>
+                </div>
+            </td>
+            <td style="width: 33%"> 
+            </td>
+        </tr>
+    </table>
 </div><!-- header -->
